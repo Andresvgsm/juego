ccd373c0047b66eea570837f2910b399175f982d
Andrés <anvesan@campusaula.com>
2025-01-27 10:41:58 +0100

    empezar con la seleccion de personaje y crear el oculto

--- inicio/inicio.js
+++ inicio/inicio.js
@@ -4,17 +4,34 @@ const boton_cargar = document.getElementById('cargar');
 
 boton_iniciar.addEventListener('click', () => {
     const divs = document.querySelectorAll('.div-animado');
-    
+
     divs.forEach((div, index) => {
         setTimeout(() => {
             div.classList.add('activo');
         }, index * 100);
     });
-
+    setTimeout(() => {
+        divs.forEach(div => {
+            div.classList.add('oculto');
+        });
+        pantalla_principal.classList.add('oculto');
+        setTimeout(() => {
+            pantalla_principal.remove();
+        }, 1000);
+        
+    }, 5000);
+    
 });
+function cambiarPantalla(idPantalla) {
+    const pantallas = document.querySelectorAll('.pantalla');
+    pantallas.forEach(pantalla => pantalla.classList.remove('activa'));
+
+    const pantallaSeleccionada = document.getElementById(idPantalla);
+    pantallaSeleccionada.classList.add('activa');
+}
 
 boton_ajustes.addEventListener('click', () => {
-    alert('Abriendo ajustes...');
+    cambiarPantalla(juego)
 });
 
 boton_cargar.addEventListener('click', () => {
