d3ce27046fffb16a1c265e9fd72dc82bafe3f924
Andrés <anvesan@campusaula.com>
2025-01-28 14:00:06 +0100

    crear seleccion de personajes y seguir la animacion

--- inicio/inicio.js
+++ inicio/inicio.js
@@ -1,8 +1,23 @@
 const boton_iniciar = document.getElementById('iniciar');
 const boton_ajustes = document.getElementById('ajustes');
 const boton_cargar = document.getElementById('cargar');
+const intro = document.getElementById('intro');
+const pantalla_principal = document.querySelector('.pantalla_principal');
+
 
 boton_iniciar.addEventListener('click', () => {
+
+    boton_iniciar.disabled = true;
+    boton_ajustes.disabled = true;
+    boton_cargar.disabled = true;
+    for (let i = 0; i < 10; i++) {
+        const img = document.createElement('img');
+        img.src = '../imagenes/nube1 (2).png';
+        img.alt = `Imagen ${i + 1}`;
+        img.className = 'div-animado div1';
+        intro.appendChild(img);
+    }
+
     const divs = document.querySelectorAll('.div-animado');
 
     divs.forEach((div, index) => {
@@ -14,21 +29,31 @@ boton_iniciar.addEventListener('click', () => {
         divs.forEach(div => {
             div.classList.add('oculto');
         });
+        
         pantalla_principal.classList.add('oculto');
         setTimeout(() => {
             pantalla_principal.remove();
+            cambiarPantalla('juego'); 
         }, 1000);
-        
     }, 5000);
-    
 });
-function cambiarPantalla(idPantalla) {
-    const pantallas = document.querySelectorAll('.pantalla');
-    pantallas.forEach(pantalla => pantalla.classList.remove('activa'));
 
-    const pantallaSeleccionada = document.getElementById(idPantalla);
-    pantallaSeleccionada.classList.add('activa');
+function cambiarPantalla(idPantalla) {
+    try {
+        const pantallas = document.querySelectorAll('.pantalla');
+        pantallas.forEach(pantalla => pantalla.classList.remove('activa'));
+
+        const pantallaSeleccionada = document.getElementById(idPantalla);
+        if (pantallaSeleccionada) {
+            pantallaSeleccionada.classList.add('activa');
+        } else {
+            console.error(`Pantalla con id "${idPantalla}" no encontrada.`);
+        }
+    } catch (error) {
+        console.error('Error al cambiar de pantalla:', error);
+    }
 }
+
 
 boton_ajustes.addEventListener('click', () => {
     cambiarPantalla(juego)
