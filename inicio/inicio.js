efbb67a34b0e1d4f1ed10c71f1de6ef495a380c1
Andrés <anvesan@campusaula.com>
2025-01-29 12:54:50 +0100

    hacer la animacion de inicio añadir imagenes a la carpeta y empezar con los personajes

--- inicio/inicio.js
+++ inicio/inicio.js
@@ -10,31 +10,85 @@ boton_iniciar.addEventListener('click', () => {
     boton_iniciar.disabled = true;
     boton_ajustes.disabled = true;
     boton_cargar.disabled = true;
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 50; i++) {
         const img = document.createElement('img');
         img.src = '../imagenes/nube1 (2).png';
-        img.alt = `Imagen ${i + 1}`;
         img.className = 'div-animado div1';
+        const posX = Math.random() * window.innerWidth*-0.1;
+        const posY = Math.random() * window.innerHeight*-0.1;
+
+        img.style.left = `${posX}px`;
+        img.style.top = `${posY}px`;
+        img.style.position = 'absolute';
+
         intro.appendChild(img);
     }
+    for (let i = 0; i < 300; i++) {
+        const img = document.createElement('img');
+        img.src = '../imagenes/nube1 (2).png';
+        img.className = 'div-animado div1';
+        const posX = Math.random() * window.innerWidth * -0.1;
+        const posY = Math.random() * window.innerHeight;
+
+        img.style.left = `${posX}px`;
+        img.style.top = `${posY}px`;
+        img.style.position = 'absolute';
+
+        intro.appendChild(img);
+    }
+    for (let i = 0; i < 100; i++) {
+        const img = document.createElement('img');
+        img.src = '../imagenes/nube1 (2).png';
+        img.className = 'div-animado div1';
+        const posX = Math.random() * window.innerWidth;
+        const posY = Math.random() * window.innerHeight * -0.1;
+
+        img.style.left = `${posX}px`;
+        img.style.top = `${posY}px`;
+        img.style.position = 'absolute';
+
+        intro.appendChild(img);
+    }
+    
+    for (let i = 0; i < 400; i++) {
+        const img = document.createElement('img');
+        img.src = '../imagenes/nube1 (2).png';
+        img.className = 'div-animado div1';
+        const posX = Math.random() * window.innerWidth * 0.9;
+        const posY = Math.random() * window.innerHeight * 0.9;
+
+        img.style.left = `${posX}px`;
+        img.style.top = `${posY}px`;
+        img.style.position = 'absolute';
+
+        intro.appendChild(img);
+    }
+    
+    
 
     const divs = document.querySelectorAll('.div-animado');
 
     divs.forEach((div, index) => {
         setTimeout(() => {
             div.classList.add('activo');
-        }, index * 100);
+        }, index * 1);
     });
     setTimeout(() => {
         divs.forEach(div => {
             div.classList.add('oculto');
         });
         
+
+        pantalla_principal.style.opacity = "0";
+        pantalla_principal.style.pointerEvents = "none";
         pantalla_principal.classList.add('oculto');
+        intro.classList.add('oculto');
+        pantalla_principal.remove();
         setTimeout(() => {
-            pantalla_principal.remove();
-            cambiarPantalla('juego'); 
+            
+            cambiarPantalla('juego');
         }, 1000);
+        
     }, 5000);
 });
 
