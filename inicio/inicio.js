08214c86de7b6f4ac0386b3cbb15120cd30c3559
andres(casa) <andresvegasanmartin@gmail.com>
2025-02-16 02:03:30 +0100

    crear las victoria y derrota, la eleccion de personaje funcionable y las imagenes necesarias, falta hacer que al pulsar un unico click sobre el personaje que quieres se extienda la explicacion

--- inicio/inicio.js
+++ inicio/inicio.js
@@ -19,10 +19,12 @@ let contadorClics = 0;
 let temporizador;
 let prota;
 let enemigo;
+let vidaMaxima = 100
 let vida_mago = 100;
-let vida_guerrero = 150;
-let vida_arquero = 120;
-let vida_enemigo = 0;
+let vida_guerrero = 100;
+let vida_arquero = 100;
+let vida_enemigo = 100;
+
 
 const intro = document.getElementById('intro');
 const pantalla_principal = document.querySelector('.pantalla_principal');
@@ -166,8 +168,9 @@ function accionClick(personaje) {
                 if (contadorClics === 1) {
                     alert("🧙‍♂️ El mago lanza un hechizo de fuego.");
                 } else if (contadorClics === 2) {
-
-                    cargarPagina('extras/seleccionEnemigo.html')
+                    prota == 'mago';
+                    cargarPagina('extras/juego_mago.html')
+                    
                 }
                 break;
 
@@ -175,7 +178,7 @@ function accionClick(personaje) {
                 if (contadorClics === 1) {
                     alert("⚔️ El guerrero golpea con su espada.");
                 } else if (contadorClics === 2) {
-                    alert("💥 El guerrero realiza un ataque especial.");
+                    cargarPagina('extras/juego_guerrero.html')
                 }
                 break;
 
@@ -183,80 +186,271 @@ function accionClick(personaje) {
                 if (contadorClics === 1) {
                     alert("🏹 El arquero dispara una flecha.");
                 } else if (contadorClics === 2) {
-                    alert("🎯 El arquero dispara una ráfaga de flechas.");
+                    cargarPagina('extras/juego_arquero.html')
                 }
                 break;
-            case "mago_enemigo":
-                if (contadorClics === 1) {
-                    alert("🧙‍♂️ El mago lanza un hechizo de fuego.");
-                } else if (contadorClics === 2) {
-                    enemigo = "mago"
-                    vida_enemigo = 100;
-                    cargarPagina('extras/juego_mago.html')
-                }
-                break;
-
-            case "guerrero_enemigo":
-                if (contadorClics === 1) {
-                    alert("⚔️ El guerrero golpea con su espada.");
-                } else if (contadorClics === 2) {
-                    alert("💥 El guerrero realiza un ataque especial.");
-                }
-                break;
-
-            case "arquero_enemigo":
-                if (contadorClics === 1) {
-                    alert("🏹 El arquero dispara una flecha.");
-                } else if (contadorClics === 2) {
-                    alert("🎯 El arquero dispara una ráfaga de flechas.");
-                }
-                break;
+            
         }
 
         contadorClics = 0;
     }, 300);
 }
 
-function inicializarAtaques(prota, ataque) {
+function reducirVida(objetivo, cantidad) {
+    if (objetivo === 'mago') {
+        vida_mago -= cantidad;
+        if (vida_mago <= 0) {
+            cargarPagina('extras/derrota.html')
 
+        }
+
+    } if (objetivo === 'guerrero') {
+        vida_guerrero -= cantidad;
+        if (vida_guerrero <= 0) {
+            cargarPagina('extras/derrota.html')
+        }
+
+    } if (objetivo === 'arquero') {
+        vida_arquero -= cantidad;
+        if (vida_arquero <= 0) {
+            cargarPagina('extras/derrota.html')
+        }
+
+    } else if (objetivo === 'enemigo') {
+        vida_enemigo -= cantidad;
+        if (vida_enemigo <= 0) {
+            cargarPagina('extras/victoria.html')
+        }
+    }
+}
+
+const vidaMaxima_enemigo = vida_enemigo;
+let vidaActual_enemigo = vidaMaxima_enemigo;
+
+let vidaActual = vidaMaxima;
+function inicializarAtaques(prota, ataque) {
     switch (prota) {
         case "mago":
-            switch (ataque) {
-                case 1:
-                    vida_enemigo -= 10;
-                    alert(`🔥 El mago lanza una bola de fuego. Vida enemigo: ${vida_enemigo}`);
-                    vida_mago -= 5;
-                    alert(`el enemigo ataca con una fuerza similar a la tuya: ${vida_mago}`);
-                    break;
-                case 2:
-                    vida_enemigo -= 15;
-                    alert(`🔥 El mago lanza una tormenta. Vida enemigo: ${vida_enemigo}`);
-                    break;
-                case 3:
-                    vida_enemigo -= 20;
-                    alert(`🔥 El mago lanza un tsunami. Vida enemigo: ${vida_enemigo}`);
-                    break;
-                case 4:
-                    vida_prota += 10;
-                    alert(`✨ El mago se cura. Vida protagonista: ${vida_prota}`);
-                    break;
-                default:
-                    alert("⚠️ Opción inválida.");
+            
+
+            
+            
+
+            
+            function actualizarBarraVida_enemigo() {
+                let porcentaje = (vidaActual_enemigo / vidaMaxima_enemigo) * 100;
+                document.getElementById("barra-vida-enemigo").style.width = porcentaje + "%";
             }
 
+            
+            function recibirDanio_enemigo(danio_enemigo ) {
+                vidaActual_enemigo -= danio_enemigo ;
+                if (vidaActual_enemigo < 0) vidaActual_enemigo = 0;
+                actualizarBarraVida_enemigo();
+            }
+
+            function curar_enemigo(vida_enemigo ) {
+                vidaActual_enemigo += vida_enemigo ;
+                if (vidaActual_enemigo > vidaMaxima_enemigo) vidaActual_enemigo = vidaMaxima_enemigo;
+                actualizarBarraVida_enemigo();
+            }
+            actualizarBarraVida_enemigo();
+            function actualizarBarraVida() {
+                let porcentaje = (vidaActual / vidaMaxima) * 100;
+                document.getElementById("barra-vida").style.width = porcentaje + "%";
+            }
+
+            function recibirDanio(danio) {
+                vidaActual -= danio;
+                if (vidaActual < 0) vidaActual = 0; 
+                actualizarBarraVida();
+            }
+
+            function curar(vida) {
+                vidaActual += vida;
+                if (vidaActual > vidaMaxima) vidaActual = vidaMaxima;
+                actualizarBarraVida();
+            }
+
+            actualizarBarraVida();
+            switch (ataque) {
+                case 1:
+                    reducirVida('enemigo', 5);
+                    alert(`🔥 El mago lanza una bola de fuego. Vida enemigo: ${vida_enemigo}`);
+                    reducirVida('mago', 10);
+                    recibirDanio(10)
+                    recibirDanio_enemigo(5)
+                    alert(`El enemigo ataca. Tu vida: ${vida_mago}`);
+                    break;
+                case 2:
+                    reducirVida('enemigo', 10);
+                    alert(`🔥 El mago lanza una tormenta. Vida enemigo: ${vida_enemigo}`);
+                    reducirVida('mago', 5);
+                    recibirDanio(5)
+                    recibirDanio_enemigo(10)
+                    alert(`El enemigo ataca. Tu vida: ${vida_mago}`);
+                    break;
+                case 3:
+                    reducirVida('enemigo', 10);
+                    alert(`🔥 El mago lanza un tsunami que no afecta como debetia al enemigo. Vida enemigo: ${vida_enemigo}`);
+                    reducirVida('mago',10);
+                    recibirDanio(10)
+                    recibirDanio_enemigo(10)
+                    alert(`El enemigo ataca. Tu vida: ${vida_mago}`);
+                    break;
+                case 4:
+                    
+                    vida_mago += 10;
+                    alert(`✨ El mago se cura. Vida mago: ${vida_mago}`);
+                    curar(10)
+                    alert(`el enemigo no consigue atacar en esta ronda`);
+                    break;
+            }
+            break;
 
         case "guerrero":
-            boton_ataque1.addEventListener('click', () => {
-                vida_enemigo -= 15;
-                alert(`⚔️ El guerrero golpea con su espada. Vida enemigo: ${vida_enemigo}`);
-            });
+            function actualizarBarraVida_enemigo() {
+                let porcentaje = (vidaActual_enemigo / vidaMaxima_enemigo) * 100;
+                document.getElementById("barra-vida-enemigo").style.width = porcentaje + "%";
+            }
+
+            
+            function recibirDanio_enemigo(danio_enemigo ) {
+                vidaActual_enemigo -= danio_enemigo ;
+                if (vidaActual_enemigo < 0) vidaActual_enemigo = 0;
+                actualizarBarraVida_enemigo();
+            }
+
+            function curar_enemigo(vida_enemigo ) {
+                vidaActual_enemigo += vida_enemigo ;
+                if (vidaActual_enemigo > vidaMaxima_enemigo) vidaActual_enemigo = vidaMaxima_enemigo;
+                actualizarBarraVida_enemigo();
+            }
+            actualizarBarraVida_enemigo();
+            function actualizarBarraVida() {
+                let porcentaje = (vidaActual / vidaMaxima) * 100;
+                document.getElementById("barra-vida").style.width = porcentaje + "%";
+            }
+
+            function recibirDanio(danio) {
+                vidaActual -= danio;
+                if (vidaActual < 0) vidaActual = 0; 
+                actualizarBarraVida();
+            }
+
+            function curar(vida) {
+                vidaActual += vida;
+                if (vidaActual > vidaMaxima) vidaActual = vidaMaxima;
+                actualizarBarraVida();
+            }
+
+            actualizarBarraVida();
+            switch (ataque) {
+                case 2:
+                    reducirVida('enemigo', 5);
+                    alert(`El guerrero realiza un ataque con su escudo que le deja un facil golpe a su enemigo. Vida enemigo: ${vida_enemigo}`);
+                    reducirVida('mago', 10);
+                    recibirDanio(10)
+                    recibirDanio_enemigo(5)
+                    alert(`El enemigo ataca. Tu vida: ${vida_mago}`);
+                    break;
+                case 3:
+                    reducirVida('enemigo', 10);
+                    alert(`El guerrero realiza un doble ataque . Vida enemigo: ${vida_enemigo}`);
+                    reducirVida('mago', 5);
+                    recibirDanio(5)
+                    recibirDanio_enemigo(10)
+                    alert(`El enemigo ataca. Tu vida: ${vida_mago}`);
+                    break;
+                case 1:
+                    reducirVida('enemigo', 10);
+                    alert(`El guerrero realiza un ataque simple. Vida enemigo: ${vida_enemigo}`);
+                    reducirVida('mago',10);
+                    recibirDanio(10)
+                    recibirDanio_enemigo(10)
+                    alert(`El enemigo ataca. Tu vida: ${vida_mago}`);
+                    break;
+                case 4:
+                    
+                    vida_mago += 10;
+                    alert(`✨ El guerrero se cura. Vida guerrero: ${vida_mago}`);
+                    curar(10)
+                    alert(`el enemigo no consigue atacar en esta ronda`);
+                    break;
+            }
             break;
 
         case "arquero":
-            boton_ataque1.addEventListener('click', () => {
-                vida_enemigo -= 8;
-                alert(`🏹 El arquero dispara una flecha. Vida enemigo: ${vida_enemigo}`);
-            });
+            function actualizarBarraVida_enemigo() {
+                let porcentaje = (vidaActual_enemigo / vidaMaxima_enemigo) * 100;
+                document.getElementById("barra-vida-enemigo").style.width = porcentaje + "%";
+            }
+
+            
+            function recibirDanio_enemigo(danio_enemigo ) {
+                vidaActual_enemigo -= danio_enemigo ;
+                if (vidaActual_enemigo < 0) vidaActual_enemigo = 0;
+                actualizarBarraVida_enemigo();
+            }
+
+            function curar_enemigo(vida_enemigo ) {
+                vidaActual_enemigo += vida_enemigo ;
+                if (vidaActual_enemigo > vidaMaxima_enemigo) vidaActual_enemigo = vidaMaxima_enemigo;
+                actualizarBarraVida_enemigo();
+            }
+            actualizarBarraVida_enemigo();
+            function actualizarBarraVida() {
+                let porcentaje = (vidaActual / vidaMaxima) * 100;
+                document.getElementById("barra-vida").style.width = porcentaje + "%";
+            }
+
+            function recibirDanio(danio) {
+                vidaActual -= danio;
+                if (vidaActual < 0) vidaActual = 0; 
+                actualizarBarraVida();
+            }
+
+            function curar(vida) {
+                vidaActual += vida;
+                if (vidaActual > vidaMaxima) vidaActual = vidaMaxima;
+                actualizarBarraVida();
+            }
+
+            actualizarBarraVida();
+            switch (ataque) {
+                case 3:
+                    reducirVida('enemigo', 5);
+                    alert(`El arquero realiza un disparo que le sale mal. Vida enemigo: ${vida_enemigo}`);
+                    reducirVida('mago', 10);
+                    recibirDanio(10)
+                    recibirDanio_enemigo(5)
+                    alert(`El enemigo ataca. Tu vida: ${vida_mago}`);
+                    break;
+                case 1:
+                    reducirVida('enemigo', 10);
+                    alert(`El arquero realiza una lluvia de flechas . Vida enemigo: ${vida_enemigo}`);
+                    reducirVida('mago', 5);
+                    recibirDanio(5)
+                    recibirDanio_enemigo(10)
+                    alert(`El enemigo ataca. Tu vida: ${vida_mago}`);
+                    break;
+                case 2:
+                    reducirVida('enemigo', 10);
+                    alert(`El arquero realiza un disparo de una unica flecha. Vida enemigo: ${vida_enemigo}`);
+                    reducirVida('mago',10);
+                    recibirDanio(10)
+                    recibirDanio_enemigo(10)
+                    alert(`El enemigo ataca. Tu vida: ${vida_mago}`);
+                    break;
+                case 4:
+                    
+                    vida_mago += 10;
+                    alert(`✨ El arquero se cura. Vida arquero: ${vida_mago}`);
+                    curar(10)
+                    alert(`el enemigo no consigue atacar en esta ronda`);
+                    break;
+            }
             break;
+
     }
 }
